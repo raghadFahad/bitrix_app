26273a1a308eb468fcd7c342b5f3c69d2ef5a1a4
raghadFahad <raghad-bajhlan@hotmail.com>
2021-02-28 17:24:34 +0300

    Update settings.php

--- settings.php
+++ settings.php
@@ -9,3 +9,4 @@ define('C_REST_IGNORE_SSL',true);//turn off validate ssl by curl
 define('C_REST_LOG_TYPE_DUMP',true); //logs save var_export for viewing convenience
 define('C_REST_BLOCK_LOG',true);//turn off default logs
 define('C_REST_LOGS_DIR', __DIR__ .'/logs/'); //directory path to save the log
+?>
